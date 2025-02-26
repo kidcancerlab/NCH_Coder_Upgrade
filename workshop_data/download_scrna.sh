8b9097b61378d6d33ac9e94c2bb6a718d5367999
MVesuviusC <matthewvc1@gmail.com>
2025-02-26 16:39:40 -0500

    Removed file that shouldn't have been committed

--- workshop_data/download_scrna.sh
+++ workshop_data/download_scrna.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-home="/igm/home/cnh008/projects/scrgot/scrgot_2025/mouse_brain/rna/raw"
-cd ${home}
+set -e
+
+my_loc=`pwd`
+
+if [ ! -d input/scRNA ]
+then
+    mkdir -p input/scRNA
+fi
+
+# We will store the raw data within input/scRNA
+cd input/scRNA/
 
 # Download data from https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE129788
 
-# download GSE193107 data that has scRNA-seq, scATAC-seq and Visium data
-wget --recursive --no-parent -nd https://ftp.ncbi.nlm.nih.gov/geo/series/GSE129nnn/GSE129788/suppl/
+# download GSE129788 data that has scRNA-seq
+wget \
+    -O GSE129788_RAW.tar \
+    "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE129788&format=file"
 
 # decompress data
 tar -xvf GSE129788_RAW.tar
+rm GSE129788_RAW.tar
+
+wget \
+    -O GSE129788_Supplementary_meta_data_Cell_Types_Etc.txt.gz \
+    "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE129788&format=file&file=GSE129788%5FSupplementary%5Fmeta%5Fdata%5FCell%5FTypes%5FEtc%2Etxt%2Egz"
+
+cd ${my_loc}
