3f14b831709e31802d0704430f1bd833affec414
MVesuviusC <matthewvc1@gmail.com>
2025-03-12 15:57:54 -0400

    Added R script with code to install R packages on the aws rstudio server

---
+++ workshop_data/pkg_installation.R
@@ -0,0 +1,96 @@
+#!/usr/bin/env Rscript
+
+# install with: dzdo Rscript test_install.R
+
+# Need to add /usr/local/lib to LD_LIBRARY_PATH so RCurl can find libiconv
+# When running this script with dzdo the LD_LIBRARY_PATH isn't the same as my version
+Sys.setenv(
+    LD_LIBRARY_PATH = paste0(
+        Sys.getenv("LD_LIBRARY_PATH"),
+        ":/usr/local/lib"
+    )
+)
+
+pkg_install_dir <- "/rstudio-workshop/apps/R/R-4.4.2_install/lib64/R/library/"
+
+cran_dependencies <-
+    c(
+        "BiocManager",
+        "pak",
+        "fs"
+    )
+
+install.packages(
+    cran_dependencies,
+    Ncpus = 4,
+    lib = pkg_install_dir,
+    repos='https://cran.rstudio.com'
+)
+
+bioc_dependencies <-
+    c(
+        "GenomeInfoDb",
+        "GenomicRanges",
+        "IRanges",
+        "Rsamtools",
+        "S4Vectors",
+        "BiocGenerics",
+        "limma",
+        "ComplexHeatmap"
+    )
+
+needed_packages_cran <-
+    c(
+        "rstudioapi",
+        "data.table",
+        "DESeq2",
+        "batchelor",
+        "fgsea",
+        "future",
+        "ggrepel",
+        "ggthemes",
+        "harmony",
+        "knitr",
+        "MAST",
+        "msigdbr",
+        "parallel",
+        "patchwork",
+        "pheatmap",
+        "RColorBrewer",
+        "Rcpp",
+        "reticulate",
+        "rmarkdown",
+        "Seurat",
+        "SeuratObject",
+        "Signac",
+        "testthat",
+        "tidyverse",
+        "terra"
+    )
+
+pak::pkg_install(
+    c(bioc_dependencies, needed_packages_cran),
+    lib = pkg_install_dir
+)
+
+
+github_packages <-
+    c(
+        "saeyslab/nichenetr",
+        "kidcancerlab/rrrSingleCellUtils",
+        "NMikolajewicz/scMiko",
+        "satijalab/seurat-data",
+        "mojaveazure/seurat-disk",
+        "satijalab/seurat-wrappers",
+        "satijalab/azimuth",
+        "drieslab/Giotto"
+    )
+
+pak::pkg_install(
+    github_packages,
+    lib = pkg_install_dir
+)
+
+# Need to chmod the library folders afterwards as default seems to be 500 for pak
+# ls -d /rstudio-workshop/apps/R/R-4.4.2_install/lib64/R/library/* | xargs dzdo chmod 555
+
