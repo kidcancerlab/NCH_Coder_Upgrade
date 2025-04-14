016894ecf6d5b0a4460c5cf6434672caad580f06
matt-gust <matt.g0017@gmail.com>
2025-04-14 16:31:41 -0400

    worked through no linked peaks being present as rownames in object

--- exploring_activity_dataset.r
+++ exploring_activity_dataset.r
@@ -10,30 +10,34 @@ activity_input_path <-
 
 h5_list <- Read10X_h5(paste0(activity_input_path,
                              "filtered_feature_bc_matrix.h5"))
-set.seed(17)
-down_index <- sample(1:ncol(h5_list[[1]]), 2000) #noling
-names(h5_list)
+
 activity_ob <-
-    CreateSeuratObject(counts = h5_list[["Gene Expression"]][ , down_index],
+    CreateSeuratObject(counts = h5_list[["Gene Expression"]],
                        assay = "RNA",
                        project = "10x_multiomics") %>%
     PercentageFeatureSet(pattern = "^MT",
                          col.name = "percent.mt_rna",
                          assay = "RNA")
 
-#Need to downsample this
-#think I'll downsample to ~2k, so I still have enough for QC
-activity_ob <- process_seurat(activity_ob)
-DimPlot(activity_ob)
-
 #going to add atac assay
 head(rownames(h5_list[["Peaks"]]))
 activity_frag <- paste0(activity_input_path, "atac_fragments.tsv.gz")
 activity_ob[["ATAC"]] <-
-    CreateChromatinAssay(counts = h5_list[["Peaks"]][ , down_index],
+    CreateChromatinAssay(counts = h5_list[["Peaks"]],
                          sep = c(":", "-"),
                          fragments = activity_frag,
                          min.cells = 0)
+
+# Need to downsample this
+set.seed(17)
+bcs_keep <- sample(colnames(activity_ob), size = 2000)
+activity_ob$keep_cell <- colnames(activity_ob) %in% bcs_keep
+activity_ob <- subset(activity_ob, keep_cell)
+
+qs::qsave(activity_ob, "testing_folder/downsampled_ob.qs")
+
+#think I'll downsample to ~2k, so I still have enough for QC
+DimPlot(activity_ob)
 
 #Add gene annotation information
 DefaultAssay(activity_ob) <- "ATAC"
@@ -73,6 +77,8 @@ activity_ob <- FRiP(activity_ob,
                     total.fragments = "total_frag",
                     col.name = "FRiP")
 
+qs::qsave(activity_ob, "testing_folder/ob_w_qc_metrics.qs")
+
 #plot qc metrics
 VlnPlot(activity_ob,
         features = c("nCount_RNA",
@@ -83,12 +89,14 @@ VlnPlot(activity_ob,
         group.by = "orig.ident",
         ncol = 5)
 
-cutoffs <- tribble(~features, ~min_val, ~max_val,
+cutoffs <- tribble(~feature, ~min_val, ~max_val,
                    "nCount_RNA", 1000, 20000,
-                   "percen.mt_rna", 0, 20,
+                   "percent.mt_rna", 0, 20,
                    "nucleosome_signal", 0, 2,
                    "TSS.enrichment", 1, 50,
                    "FRiP", 0.25, 1)
+
+feature_hist(activity_ob, features = cutoffs$feature, cutoff_table = cutoffs)
 
 activity_ob <- subset(activity_ob,
                       nCount_RNA %in% c(1000:20000) &
@@ -139,12 +147,17 @@ activity_small <- subset(activity_ob, ATAC_cluster %in% c(0, 1)) %>%
             dims = 2:30,
             reduction.name = "umap_atac")
 
+qs::qsave(activity_small, "testing_folder/activity_small.qs")
+
 #find differentially accessible peaks
 Idents(activity_small) <- activity_small$ATAC_cluster
 
+r_dim_plot(activity_small, reduction = "umap_rna", group.by = "RNA_cluster") |
+r_dim_plot(activity_small, reduction = "umap_atac", group.by = "ATAC_cluster")
+
 diff_peaks <- FindAllMarkers(activity_small,
                           assay = "ATAC",
-                          min.pct = .2) %>%
+                          min.pct = .1) %>%
     subset(p_val_adj < 0.05)
 
 top_5_each <- group_by(diff_peaks, cluster) %>%
@@ -153,3 +166,63 @@ top_5_each <- group_by(diff_peaks, cluster) %>%
 
 #plot peaks as atac feature plot
 r_feature_plot(activity_ob, features = top_5_each$gene)
+
+#Link peaks to genes
+#First I'm going to find differentially expressed genes between ATAC clusters, bc we're only interested in genes whose expression is changing between states
+DefaultAssay(activity_small) <- "RNA"
+activity_small <- process_seurat(activity_small)
+activity_small$RNA_cluster <- activity_small$seurat_clusters
+Idents(activity_small) <- activity_small$RNA_cluster
+degs <- FindAllMarkers(activity_small,
+                       assay = "RNA",
+                       only.pos = TRUE,
+                       min.pct = 0.1) %>%
+    subset(p_val_adj < 0.05 & avg_log2FC > 1)
+dim(degs)
+
+#link peaks to genes; this returns a seurat object
+#have to run region stats first
+DefaultAssay(activity_small) <- "ATAC"
+start <- Sys.time()
+activity_small <-
+    RegionStats(activity_small,
+                genome = BSgenome.Hsapiens.UCSC.hg38::BSgenome.Hsapiens.UCSC.hg38) #nolint
+Sys.time() - start
+
+#Link peaks takes about 7 minutes
+start <- Sys.time()
+activity_small <- LinkPeaks(object = activity_small,
+                            peak.assay = "ATAC",
+                            expression.assay = "RNA",
+                            genes.use = rownames(degs)[1:200])
+Sys.time() - start
+qs::qsave(activity_small, "testing_folder/activity_small.qs")
+
+#look at peaks
+links <- Links(activity_small) %>%
+    as.data.frame() %>%
+    as_tibble() %>%
+    arrange(pvalue)
+head(links)
+
+#lets see if any of our top 5 peaks are in here
+subset(links, peak %in% top_5_each$gene)
+r_feature_plot(activity_ob, reduction = "umap_rna", features = "KCTD8") |
+r_dim_plot(activity_ob, reduction = "umap_rna", group.by = c("RNA_cluster", "ATAC_cluster"))
+table(activity_small$RNA_cluster, activity_small$ATAC_cluster)
+
+##
+# NO LINKED PEAKS FOUND IN OBJECT!! HOW?????
+# ANSWER: YOUR DUMB ASS WASN'T USING THE peak COLUMN FROM THE LINKPEAKS OUTPUT
+##
+
+#lets try closest feature instead and filter against degs
+close_genes <- ClosestFeature(activity_small, regions = diff_peaks$gene)
+head(close_genes)
+
+subset(close_genes,
+       gene_name %in% degs$gene &
+       distance == 0) %>%
+    pull(gene_name)
+
+#motif analysis
