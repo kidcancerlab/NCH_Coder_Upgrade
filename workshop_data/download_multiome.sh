dd54f02366abe8b335d50ace6aacd52315dd8105
matt-gust <matt.g0017@gmail.com>
2025-04-22 15:54:28 -0400

    full shell script to download data and put it in the proper folders

--- workshop_data/download_multiome.sh
+++ workshop_data/download_multiome.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 my_loc=$(pwd)
 
-set -e
+# set -e
 
 if [ ! -d input/multiomics ]
 then
     mkdir -p input/multiomics
+fi
+
+if [ ! -d input/multiomics/activity_data ]
+then
+    mkdir input/multiomics/activity_data/
+fi
+
+if [ ! -d input/multiomics/downsampled_data ]
+then
+    mkdir input/multiomics/downsampled_data
 fi
 
 cd input/multiomics
@@ -15,28 +25,59 @@ cd input/multiomics
 
 # Output Files
 # wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_cloupe.cloupe
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_analysis.tar.gz
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_web_summary.html
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_summary.csv
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_analysis.tar.gz
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_web_summary.html
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_summary.csv
 wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_per_barcode_metrics.csv
 wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_filtered_feature_bc_matrix.tar.gz
 wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_filtered_feature_bc_matrix.h5
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_raw_feature_bc_matrix.tar.gz
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_raw_feature_bc_matrix.h5
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_raw_feature_bc_matrix.tar.gz
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_raw_feature_bc_matrix.h5
 # wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_gex_possorted_bam.bam
 # wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_gex_possorted_bam.bam.bai
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_gex_molecule_info.h5
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_gex_molecule_info.h5
 # wget https://s3-us-west-2.amazonaws.com/10x.files/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_possorted_bam.bam
 # wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_possorted_bam.bam.bai
 wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_fragments.tsv.gz
 wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_fragments.tsv.gz.tbi
 wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_peaks.bed
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_cut_sites.bigwig
-wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_peak_annotation.tsv
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_cut_sites.bigwig
+# wget https://cf.10xgenomics.com/samples/cell-arc/2.0.0/human_brain_3k/human_brain_3k_atac_peak_annotation.tsv
+
+#move this data into activity_data
+mv human_brain* activity_data/
+
 
 #Download the activity dataset
 # cultured neurons treated with KCI, measured 1 hour post stimulation
 wget https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8793nnn/GSM8793216/suppl/GSM8793216%5FDuan%5F029%5F20088%2D1%5Finput%5Flib%5Fhg38only.tar.gz
 tar -xf GSM8793216_Duan_029_20088-1_input_lib_hg38only.tar.gz
 
+
 cd ${my_loc}
+
+cd input/multiomics/Duan_029_20088-1_input_lib_hg38only/outs
+
+echo randomly sampling 2000 barcodes
+ml R/4.3.0
+Rscript ${my_loc}/workshop_data/downsample_multiome_activity.r
+
+gunzip atac_fragments.tsv.gz
+cp atac_fragments.tsv /home/gdrobertslab/lab/Analysis/MattGust/projects/non_lab/25_coder_upgrade/input/multiomics/downsampled_data/atac_fragments.tsv
+
+cd ${my_loc}/input/multiomics/downsampled_data
+#write the header to a new fragments file first
+grep "#" atac_fragments.tsv > new_fragments.tsv
+#now cat all lines containing the selected barcodes
+grep -f downsampled_bcs.tsv atac_fragments.tsv >> new_fragments.tsv
+ml SAMtools
+bgzip new_fragments.tsv
+
+#delete old fragment file
+# rm atac_fragments.tsv
+
+tabix -p bed new_fragments.tsv.gz
+
+cd ${my_loc}
+
+Rscript workshop_data/make_activity_small.r
