af03578c3c79118391f3f3ca83e81029008b1117
mvesuviusc <matthewvc1@gmail.com>
2026-06-05 14:20:16 +0000

    Updating package install directory and rstudio preferences

--- workshop_data/pkg_installation.R
+++ workshop_data/pkg_installation.R
@@ -10,17 +10,17 @@
 # Need to add /usr/local/lib to LD_LIBRARY_PATH so RCurl can find libiconv
 # When running this script with dzdo the LD_LIBRARY_PATH isn't the same as my version
 Sys.setenv(
-    LD_LIBRARY_PATH = paste0(
-        Sys.getenv("LD_LIBRARY_PATH"),
-        ":/usr/local/lib"
-    )
+  LD_LIBRARY_PATH = paste0(
+    Sys.getenv("LD_LIBRARY_PATH"),
+    ":/usr/local/lib"
+  )
 )
 
 ## Libraries needed to be installed:
 # libuv
 # xml2
 # libgsl
-# libgdal 
+# libgdal
 # libgeos
 # libproj
 # libtbb
@@ -37,79 +37,82 @@ Sys.setenv(
 ### libwebp  dzdo yum install libjpeg-devel libwebp-devel
 ### cairo    dzdo yum install cairo-devel
 
-pkg_install_dir <- "/rstudio-workshop/apps/R/R-4.5.3/library/"
+# Make sure this matches .libPaths()
+# Also double check the chmod at the end
+pkg_install_dir <- "/rstudio-workshop/apps/R/R-4.5.3/lib64/R/library"
 
 cran_dependencies <-
-    c(
-        "BiocManager",
-        "pak",
-        "fs"
-    )
+  c(
+    "BiocManager",
+    "pak",
+    "fs"
+  )
 
 install.packages(
-    cran_dependencies,
-    Ncpus = 4,
-    lib = pkg_install_dir,
-    repos = "https://cran.rstudio.com"
+  cran_dependencies,
+  Ncpus = 4,
+  lib = pkg_install_dir,
+  repos = "https://cran.rstudio.com"
 )
 
 bioc_dependencies <-
-    c(
-        "GenomeInfoDb",
-        "GenomicRanges",
-        "IRanges",
-        "Rsamtools",
-        "S4Vectors",
-        "BiocGenerics",
-        "limma",
-        "ComplexHeatmap",
-        "ensembldb",
-        "SPOTlight",
-        "biomaRt",
-        "DropletUtils",
-        "multtest",
-        "metap",
-        "biovizBase",
-        "multtest"
-    )
+  c(
+    "GenomeInfoDb",
+    "GenomicRanges",
+    "IRanges",
+    "Rsamtools",
+    "S4Vectors",
+    "BiocGenerics",
+    "limma",
+    "ComplexHeatmap",
+    "ensembldb",
+    "SPOTlight",
+    "biomaRt",
+    "DropletUtils",
+    "multtest",
+    "metap",
+    "biovizBase",
+    "multtest"
+  )
 
 needed_packages_cran <-
-    c(
-        "devtools",
-        "rstudioapi",
-        "data.table",
-        "DESeq2",
-        "batchelor",
-        "future",
-        "ggrepel",
-        "ggthemes",
-        "harmony",
-        "hdf5r",
-        "knitr",
-        "MAST",
-        "patchwork",
-        "pheatmap",
-        "RColorBrewer",
-        "Rcpp",
-        "reticulate",
-        "rmarkdown",
-        "Seurat",
-        "SeuratObject",
-        "testthat",
-        "tidyverse",
-        "terra",
-        "anndata",
-        "scatterpie",
-        "qs2"
-    )
+  c(
+    "devtools",
+    "rstudioapi",
+    "data.table",
+    "DESeq2",
+    "batchelor",
+    "future",
+    "ggrepel",
+    "ggthemes",
+    "harmony",
+    "hdf5r",
+    "knitr",
+    "MAST",
+    "patchwork",
+    "pheatmap",
+    "RColorBrewer",
+    "Rcpp",
+    "reticulate",
+    "rmarkdown",
+    "Seurat",
+    "SeuratObject",
+    "testthat",
+    "tidyverse",
+    "terra",
+    "anndata",
+    "scatterpie",
+    "qs2",
+    "scrapper"
+  )
 
 #Doing this one at a time so I can see which one fails
 for (this_pkg in c(bioc_dependencies, needed_packages_cran)) {
   message("Now installing: ", this_pkg)
-  
+
   pak::pkg_install(
-      this_pkg,
-      lib = pkg_install_dir
+    this_pkg,
+    lib = pkg_install_dir
   )
 }
 
@@ -117,23 +120,26 @@ for (this_pkg in c(bioc_dependencies, needed_packages_cran)) {
 remotes::install_github("stuart-lab/signac", ref = "1.16.0")
 
 github_packages <-
-    c(
-        "igordot/msigdbr",
-        
-        "alserglab/fgsea",
-        "saeyslab/nichenetr",
-        "kidcancerlab/rrrSingleCellUtils",
-        "NMikolajewicz/scMiko",
-        "satijalab/seurat-data",
-        "mojaveazure/seurat-disk",
-        "satijalab/seurat-wrappers",
-        "satijalab/azimuth",
-        "drieslab/Giotto",
-        "10xGenomics/loupeR",
-        "jinworks/CellChat",
-        "immunogenomics/crescendo",
-        "dmcable/spacexr"
-    )
+  c(
+    "igordot/msigdbr",
+    "alserglab/fgsea",
+    "saeyslab/nichenetr",
+    "kidcancerlab/rrrSingleCellUtils",
+    "NMikolajewicz/scMiko",
+    "satijalab/seurat-data",
+    "mojaveazure/seurat-disk",
+    "satijalab/seurat-wrappers",
+    "satijalab/azimuth",
+    "drieslab/Giotto",
+    "10xGenomics/loupeR",
+    "jinworks/CellChat",
+    "JEFworks-Lab/MERINGUE",
+    "KlugerLab/ALRA",
+    "jinworks/SpatialCellChat",
+    "immunogenomics/crescendo",
+    "dmcable/spacexr",
+    "MVesuviusC/ramTrackR"
+  )
 
 for (this_pkg in github_packages) {
   message("Now installing: ", this_pkg)
@@ -144,8 +150,11 @@ for (this_pkg in github_packages) {
   )
 }
 
+# Need to get a specific version of harmony to get around an issue (https://github.com/immunogenomics/harmony/issues/284)
+devtools::install_github("pati-ni/harmony", ref="lapack-linux-fix", force=TRUE)
+
 # This failed when installed with pak
-# Error was 
+# Error was
 # ! error in pak subprocess
 # Caused by error in `stop_task_package_uncompress(state, worker)`:
 #   ! Failed to uncompress BPCells from
@@ -171,10 +180,10 @@ BiocManager::install("motifmatchr")
 
 
 # Need to chmod the library folders afterwards as default seems to be 500 for pak
-# ls -d /rstudio-workshop/apps/R/R-4.5.3/library/* | xargs dzdo chmod 555
+# ls -d /rstudio-workshop/apps/R/R-4.5.3/lib64/R/library/* | xargs dzdo chmod 555
 
 warning(
-    "Remember to run `ls -d /rstudio-workshop/apps/R/R-4.5.3/library/* | xargs dzdo chmod 555`"
+  "Remember to run `ls -d /rstudio-workshop/apps/R/R-4.5.3/lib64/R/library/* | xargs dzdo chmod 555`"
 )
 
 # For rstudio server, modify the server's global settings
@@ -183,7 +192,8 @@ warning(
 #     "save_workspace": "never",
 #     "load_workspace": false,
 #     "knit_working_dir": "project",
-#     "rmd_chunk_output_inline": false
+#     "rmd_chunk_output_inline": false,
+#     "graphics_backend": "ragg"
 # }
 
 # Remember to chmod 644 on the file so user's instances can read it
